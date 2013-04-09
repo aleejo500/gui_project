0c5d884a915cebfb41a61f179f03efe27896c148
Ahmed Mf <a.mahfouf@gmail.com>
2013-04-09 14:05:47 +0200

    Ajout du listener pour le bouton Rechercher.

--- src/Gui/Recherche.java
+++ src/Gui/Recherche.java
@@ -9,11 +9,22 @@ import javax.swing.JTextField;
 /** Panel correspondant à l'onglet 
     de recherche d'une station de metro */
 public class Recherche extends JPanel {
+    /** Bouton qui lance la recherche */
+    JButton searchButton;
+    /** Le champ de texte dans lequel sera entré la recherche */
+    JTextField searchField;
+
     /** Constructeur sans argument */
     public Recherche () {
 	super(new FlowLayout());
-	this.add(new JLabel("Recherche : "));
-	this.add(new JTextField(30));
-	this.add(new JButton("Rechercher").addActionListener(new RechercheListener()));
+
+	searchButton = new JButton("Rechercher");
+	searchButton.addActionListener(new RechercheListener());
+
+	searchField = new JTextField(30);
+
+	add(new JLabel("Recherche : "));
+	add(searchField);
+	add(searchButton);
     }
 }
