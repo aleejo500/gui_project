9d08da10761b1321a500ae31b2f6d4c44ad899ae
Ahmed Mf <a.mahfouf@gmail.com>
2013-04-09 19:45:25 +0200

    Utilisation du CardLayout pour sélectionner le type de recherche à effectuer.

--- src/Gui/Recherche.java
+++ src/Gui/Recherche.java
@@ -1,17 +1,52 @@
 package Gui;
 
-import javax.swing.BoxLayout;
+import java.awt.CardLayout;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 import javax.swing.JScrollPane;
+import javax.swing.JComboBox;
+import javax.swing.BoxLayout;
 
 /** Panel correspondant à l'onglet 
     de recherche d'une station de metro */
-public class Recherche extends JPanel {
+public class Recherche extends JPanel implements ItemListener {
+    final static String TEXTUELLE = "Nom de station";
+    final static String LIGNE = "Numéro de ligne";
+    final static String APPROX = "Localisation approximative";
+    private JPanel cards;
+
     /** Constructeur sans argument */
     public Recherche () {
+	/* Creation de la combo box */
+	JPanel comboBoxPane = new JPanel();
+	String [] cbitems = {TEXTUELLE, LIGNE, APPROX};
+	JComboBox cb = new JComboBox(cbitems);
+	cb.setEditable(false);
+	cb.addItemListener(this);
+	comboBoxPane.add(cb);
+
+	/* JPanel de la selection */
+	JPanel select = new JPanel();
+	select.add(new JLabel("Selectionnez le type de recherche : "));
+	select.add(comboBoxPane);
+
+	/* Panel de la fonction de recherche */
+	cards = new JPanel(new CardLayout());
+	cards.add(new RechercheText(), TEXTUELLE);
+	cards.add(new RechercheList(), LIGNE);
+
+	/* Ajout au panel courant */
 	setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-	/* Ajout des composants */
-	add(new RechercheText());
-	add(new RechercheList());
+	add(select);
+	add(cards);
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent evt) {
+	CardLayout cl = (CardLayout)(cards.getLayout());
+	cl.show(cards, (String)evt.getItem());
     }
 }
