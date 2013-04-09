5908b8cb184a8b965bce914661ecd89d54b1aee2
Ahmed M <a.mahfouf@gmail.com>
2013-04-09 18:28:54 +0200

    Composant contenant toutes les manières de faire des recherches.

--- src/Gui/Recherche.java
+++ src/Gui/Recherche.java
@@ -1,35 +1,17 @@
 package Gui;
 
-import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import javax.swing.JTextField;
 import javax.swing.JScrollPane;
 
 /** Panel correspondant à l'onglet 
     de recherche d'une station de metro */
 public class Recherche extends JPanel {
-    /** Bouton qui lance la recherche */
-    JButton searchButton;
-    /** Le champ de texte dans lequel sera entré la recherche */
-    JTextField searchField;
-    /** Le champ dans lequel on peut selectionner la ligne */
-    JScrollPane scrollLigne;
-
     /** Constructeur sans argument */
     public Recherche () {
-	super(new FlowLayout());
-
-	searchButton = new JButton("Rechercher");
-	searchButton.addActionListener(new RechercheListener());
-	searchField = new JTextField(30);
-	scrollLigne = new JScrollPane(new LigneList());
-
+	setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 	/* Ajout des composants */
-	add(new JLabel("Recherche : "));
-	add(searchField);
-	add(searchButton);
-	add(scrollLigne);
+	add(new RechercheText());
+	add(new RechercheList());
     }
 }
