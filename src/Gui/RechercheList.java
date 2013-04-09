54b710efc4b75e72d968856733b06195403d0444
Ahmed Mf <a.mahfouf@gmail.com>
2013-04-09 22:01:58 +0200

    Ajout d'une liste pour les stations.

--- src/Gui/RechercheList.java
+++ src/Gui/RechercheList.java
@@ -1,15 +1,28 @@
 package Gui;
 
 import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JLabel;
 
 /** Panel correspondant à une recherche en 
     selectionnant dans une JList */
 public class RechercheList extends JPanel {
     /** Constructeur sans argument */
     public RechercheList() {
-	super(new FlowLayout());
-	add(new JScrollPane(new LigneList()));
+	JPanel lignes = new JPanel();
+	JPanel stations = new JPanel();
+
+	lignes.setLayout(new BoxLayout(lignes, BoxLayout.Y_AXIS));
+	lignes.add(new JLabel("Numéro de la ligne"));
+	lignes.add(new JScrollPane(new LigneList()));
+
+	stations.setLayout(new BoxLayout(stations, BoxLayout.Y_AXIS));
+	stations.add(new JLabel("Nom de la station"));
+	stations.add(new JScrollPane(new LigneList()));
+
+	add(lignes);
+	add(stations);
     }
 }
