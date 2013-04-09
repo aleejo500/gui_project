35937b3fc673c06efee0ae6005b1c3c7ab95ef7c
Ahmed Mf <a.mahfouf@gmail.com>
2013-04-09 15:04:00 +0200

    Modification des ActionListener pour qu'il écrivent sur la sortie standard.

---
+++ src/Gui/ItineraireListener.java
@@ -0,0 +1,10 @@
+package Gui;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ItineraireListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+	System.out.println("Oh oui !");
+    }
+}
