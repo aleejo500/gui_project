ec8d2e8981f9ebcfc67167f865cb6a166810faa1
Ahmed Mf <a.mahfouf@gmail.com>
2013-04-09 14:49:54 +0200

    Ajout d'un ActionListener pour le bouton de recherche d'itinéraire.

--- src/Gui/Itineraire.java
+++ src/Gui/Itineraire.java
@@ -9,13 +9,28 @@ import javax.swing.JTextField;
 /** Panel correspondant à l'onglet 
     de recherche d'une station de metro */
 public class Itineraire extends JPanel {
+    /** Bouton qui permet de lancer la recherche d'itinéraire */
+    private JButton itineraireButton;
+    /** Champ de texte qui permet d'entrer le départ */
+    private JTextField departField;
+    /** Champ de texte qui permet d'entrer l'arrivé */
+    private JTextField arriveeField;
+
     /** Constructeur sans argument */
     public Itineraire () {
+	/* On applique le FlowLayout à notre JComponent */
 	super(new FlowLayout());
-	this.add(new JLabel("Depart : "));
-	this.add(new JTextField(30));
-	this.add(new JLabel("Arrivé : "));
-	this.add(new JTextField(30));
-	this.add(new JButton("Tappe moi !"));
+
+	itineraireButton = new JButton("Tappe moi !");
+	itineraireButton.addActionListener(new ItineraireListener());
+
+	departField = new JTextField(30);
+	arriveeField = new JTextField(30);
+
+	add(new JLabel("Départ : "));
+	add(departField);
+	add(new JLabel("Arrivée : "));
+	add(arriveeField);
+	add(itineraireButton);
     }
 }
