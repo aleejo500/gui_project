b1b8351ad2d7d6257ddc168ed7bcc08cb24d40ec
Ahmed M <a.mahfouf@gmail.com>
2013-04-09 17:33:10 +0200

    Ajout d'une JList scrollable

--- src/Gui/Recherche.java
+++ src/Gui/Recherche.java
@@ -5,6 +5,7 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.JScrollPane;
 
 /** Panel correspondant à l'onglet 
     de recherche d'une station de metro */
@@ -13,6 +14,8 @@ public class Recherche extends JPanel {
     JButton searchButton;
     /** Le champ de texte dans lequel sera entré la recherche */
     JTextField searchField;
+    /** Le champ dans lequel on peut selectionner la ligne */
+    JScrollPane scrollLigne;
 
     /** Constructeur sans argument */
     public Recherche () {
@@ -20,11 +23,13 @@ public class Recherche extends JPanel {
 
 	searchButton = new JButton("Rechercher");
 	searchButton.addActionListener(new RechercheListener());
-
 	searchField = new JTextField(30);
+	scrollLigne = new JScrollPane(new LigneList());
 
+	/* Ajout des composants */
 	add(new JLabel("Recherche : "));
 	add(searchField);
 	add(searchButton);
+	add(scrollLigne);
     }
 }
