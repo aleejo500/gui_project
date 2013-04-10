8d54c39798d49b044aa3a62268dca8a6419ffb02
Ahmed Mf <a.mahfouf@gmail.com>
2013-04-10 17:56:36 +0200

    Ajout du panel pour la recherche approximative.

---
+++ src/Gui/RechercheApprox.java
@@ -0,0 +1,28 @@
+package Gui;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+/** Panel correspondant à la recherche 
+    approximative d'une station de metro */
+public class RechercheApprox extends JPanel {
+    /** Bouton qui lance la recherche */
+    JButton searchButton;
+    /** Le champ de texte dans lequel sera entré la recherche */
+    JTextField searchField;
+
+    /** Constructeur sans argument */
+    public RechercheApprox () {
+		/* Création des composants */
+		searchButton = new JButton("Rechercher");
+		searchButton.addActionListener(new RechercheListener());
+		searchField = new JTextField(30);
+
+		/* Ajout des composants */
+		add(new JLabel("Localisation : "));
+		add(searchField);
+		add(searchButton);
+    }
+}
