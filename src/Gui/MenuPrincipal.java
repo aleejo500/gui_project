b0f5385c101d3661ebd38413bb903f90de5c1d26
Ahmed Mf <a.mahfouf@gmail.com>
2013-04-13 16:04:30 +0200

    Menu réduit à une seule entrée Fichier avec la fonctionnalité Quitter implémentée

--- src/Gui/MenuPrincipal.java
+++ src/Gui/MenuPrincipal.java
@@ -3,23 +3,26 @@ package Gui;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /** Barre de menu de la fenêtre principale */
 public class MenuPrincipal extends JMenuBar {
     /** Constructeur sans argument */
     public MenuPrincipal() {
 	super();
-	JMenu [] menus = {
-	    new JMenu("Fichier"),
-	    new JMenu("Édition"),
-	    new JMenu("Outils")};
+	JMenu file = new JMenu("Fichier");
+
+	JMenuItem quit = new JMenuItem("Quitter");
+	quit.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+		    System.exit(0);
+		}
+	    });
 	
-	for(int i = 0; i < menus.length; i++) {
-	    JMenu tmp = menus[i];
-	    tmp.add(new JMenuItem("Premier"));
-	    tmp.add(new JMenuItem("Deuxième"));
-	    tmp.add(new JMenuItem("Troisième"));
-	    this.add(tmp);
-	}
+	file.add(quit);
+	
+	add(file);
     }
 }
+
