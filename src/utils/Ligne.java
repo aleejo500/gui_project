f19219bbdc9c4efcebe7409672e4e8951f962b8a
Ahmed Mf <a.mahfouf@gmail.com>
2013-05-02 13:10:56 +0200

    Redéfinition de la méthode toString()

--- src/utils/Ligne.java
+++ src/utils/Ligne.java
@@ -18,6 +18,7 @@ public class Ligne {
 
     /**
      * Le constructeur
+     *
      * @param numero Le numero extrait de la base de donnée.
      * @param type Le type extrait de la base de donnée.
      */
@@ -29,6 +30,7 @@ public class Ligne {
 
     /**
      * Getter pour le numero de la ligne.
+     *
      * @return Le numero de ligne tel qu'il est extrait de la base de donnée.
      */
     public String getNumero() {
@@ -37,6 +39,7 @@ public class Ligne {
 
     /**
      * Getter pour l'identifiant de la ligne.
+     *
      * @return L'identifiant de la ligne.
      */
     public String getId() {
@@ -45,26 +48,33 @@ public class Ligne {
 
     /**
      * Getter pour le type de la ligne.
+     *
      * @return Le type de la ligne
      */
     public String getType() {
         return type;
     }
-    
+
     /**
      * Analyse le numéro de la base de donnée pour extraire l'id de la ligne.
+     *
      * @param num Le numero extrait de la base de donnée.
      * @return L'identifiant de la ligne.
      */
     private String processNumero(String num) {
         StringBuilder builder = new StringBuilder();
-        
+
         int i = 0;
-        while((num.charAt(i) != ' ') && (i < num.length())) {
+        while ((num.charAt(i) != ' ') && (i < num.length())) {
             builder.append(num.charAt(i));
             i++;
         }
-        
+
         return builder.toString();
+    }
+
+    @Override
+    public String toString() {
+        return id;
     }
 }
