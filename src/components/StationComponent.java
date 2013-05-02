a73e0eeb6b0068b026fdfae79ec8a8d98f2d1d43
Ahmed Mf <a.mahfouf@gmail.com>
2013-05-02 11:54:43 +0200

    Ajout d'un fichier pour gérer la représentation des stations sur le plan de métro

---
+++ src/components/StationComponent.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package components;
+
+import javax.swing.JCheckBox;
+
+/**
+ *
+ * @author amidouch
+ */
+public class StationComponent extends JCheckBox {
+
+    public StationComponent(int x, int y) {
+        setBounds(x - 10, y - 10, 20, 20);
+    }
+}
