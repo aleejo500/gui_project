82647804e851b48c1be4a4aab030c7da07e3caf5
Ahmed Mf <a.mahfouf@gmail.com>
2013-05-02 13:11:56 +0200

    Utilisation des objets du packag utils au lieu de manipuler des String

--- src/components/StationList.java
+++ src/components/StationList.java
@@ -3,35 +3,32 @@ package components;
 import javax.swing.JList;
 import javax.swing.DefaultListModel;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import utils.DBConnection;
-import panels.Recherche;
 import utils.Station;
 import java.util.LinkedList;
 
 /** Classe permettant d'utiliser une 
 liste avec les stations de metro */
-public class StationList extends JList<String> {
+public class StationList extends JList<Station> {
 
-    private DefaultListModel<String> listModel;
+    private DefaultListModel<Station> listModel;
     private LinkedList<Station> stations;
 
     /** Contructeur sans argument */
     public StationList() {
-        listModel = new DefaultListModel<String>();
+        listModel = new DefaultListModel<Station>();
         try {
             DBConnection con = new DBConnection();
             stations = con.getMetroStations();
             while (!stations.isEmpty()) {
-                listModel.addElement(stations.pop().getNom());
+                listModel.addElement(stations.pop());
             }
         } catch (SQLException ex) {
-            Logger.getLogger(Recherche.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println(ex);
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Recherche.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println(ex);
         } catch (Exception ex) {
-            Logger.getLogger(Recherche.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println(ex);
         }
         setModel(listModel);
     }
