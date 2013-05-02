f10a1fe7c1079c0e029cf122d8d336ec5f59407c
Ahmed Mf <a.mahfouf@gmail.com>
2013-05-02 12:46:03 +0200

    Nouvelle manière de gérer les lignes.

--- src/utils/Ligne.java
+++ src/utils/Ligne.java
@@ -3,22 +3,68 @@ package utils;
 /* Classe qui modélise une ligne de transport */
 public class Ligne {
 
-    /** L'identifiant de la ligne */
-    private int id;
-    /** Le nom de la ligne */
-    private String nom;
-    /** Le type de ligne : metro, bus, rer, tramway ... */
+    /**
+     * Identifiant de la ligne.
+     */
+    private String id;
+    /**
+     * Le numéro de la ligne extrait de la base de donnée
+     */
+    private String numero;
+    /**
+     * Le type de ligne : metro, bus, rer, tramway ...
+     */
     private String type;
 
-    /** Constructeur */
-    public Ligne(int id, String nom, String type) {
-        this.id = id;
-        this.nom = nom;
+    /**
+     * Le constructeur
+     * @param numero Le numero extrait de la base de donnée.
+     * @param type Le type extrait de la base de donnée.
+     */
+    public Ligne(String numero, String type) {
+        this.numero = numero;
+        this.id = processNumero(numero);
         this.type = type;
     }
 
-    /** Retourne le nom de la ligne */
-    public String getNom() {
-        return nom;
+    /**
+     * Getter pour le numero de la ligne.
+     * @return Le numero de ligne tel qu'il est extrait de la base de donnée.
+     */
+    public String getNumero() {
+        return numero;
+    }
+
+    /**
+     * Getter pour l'identifiant de la ligne.
+     * @return L'identifiant de la ligne.
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Getter pour le type de la ligne.
+     * @return Le type de la ligne
+     */
+    public String getType() {
+        return type;
+    }
+    
+    /**
+     * Analyse le numéro de la base de donnée pour extraire l'id de la ligne.
+     * @param num Le numero extrait de la base de donnée.
+     * @return L'identifiant de la ligne.
+     */
+    private String processNumero(String num) {
+        StringBuilder builder = new StringBuilder();
+        
+        int i = 0;
+        while((num.charAt(i) != ' ') && (i < num.length())) {
+            builder.append(num.charAt(i));
+            i++;
+        }
+        
+        return builder.toString();
     }
 }
