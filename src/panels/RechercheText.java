9f0cd93daba9a74ee98061ed2a39e458c7a07453
Ahmed Mf <a.mahfouf@gmail.com>
2013-05-03 15:06:19 +0200

    JTextField remplacé par un JComboBox éditable pour entrer les stations de métro

--- src/panels/RechercheText.java
+++ src/panels/RechercheText.java
@@ -1,22 +1,25 @@
 package panels;
 
+import java.awt.Dimension;
 import java.util.ResourceBundle;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
-import javax.swing.JTextField;
+import javax.swing.JComboBox;
 import listeners.RechercheListener;
+import listeners.SearchStationListener;
 
 /** Panel correspondant à l'onglet 
 de recherche d'une station de metro */
 public class RechercheText extends JPanel {
 
-    /** Bouton qui lance la recherche */
+    /** Bouton qui lance la recherche. */
     JButton searchButton;
-    /** Le champ de texte dans lequel sera entré la recherche */
-    JTextField searchField;
+    /** Le champ de texte dans lequel sera entré la recherche. */
+    JComboBox<String> searchField;
 
     /** Constructeur sans argument */
     public RechercheText() {
@@ -31,7 +34,11 @@ public class RechercheText extends JPanel {
         /* Création des composants */
         searchButton = new JButton(button.getString("textSearchButton"));
         searchButton.addActionListener(new RechercheListener());
-        searchField = new JTextField(30);
+        searchField = new JComboBox<String>(new DefaultComboBoxModel<String>());
+        searchField.setPreferredSize(new Dimension(350, 20));
+        searchField.setEditable(true);
+        searchField.getEditor().getEditorComponent()
+                .addKeyListener(new SearchStationListener(searchField));
 
         /* Placement des composants */
         c.gridx = 0;
@@ -41,12 +48,13 @@ public class RechercheText extends JPanel {
 
         c.gridx = 1;
         c.gridy = 0;
-        c.fill = GridBagConstraints.BOTH;
+        c.gridwidth = 2;
+        c.fill = GridBagConstraints.HORIZONTAL;
         add(searchField, c);
 
         c.gridx = 0;
         c.gridy = 1;
-        c.gridwidth = 2;
+        c.gridwidth = 3;
         c.fill = GridBagConstraints.BOTH;
         add(searchButton, c);
     }
