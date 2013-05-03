74dfa51acd8c223224b96054231e36c4567bd8cc
Ahmed Mf <a.mahfouf@gmail.com>
2013-05-03 16:59:05 +0200

    Modification de la structure de l'objet.

--- src/components/StationComponent.java
+++ src/components/StationComponent.java
@@ -11,8 +11,13 @@ import javax.swing.JCheckBox;
  * @author amidouch
  */
 public class StationComponent extends JCheckBox {
+    private static final int DIAMETRE = 20;
+    private int x;
+    private int y;
 
     public StationComponent(int x, int y) {
-        setBounds(x - 10, y - 10, 20, 20);
+        this.x = x - (DIAMETRE / 2);
+        this.y = y - (DIAMETRE / 2);
+        setBounds(this.x, this.y, DIAMETRE, DIAMETRE);
     }
 }
